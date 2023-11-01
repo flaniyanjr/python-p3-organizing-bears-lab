d8a3d626f80d8627f4ba538be8f6f8ae113a2826
Felix Laniyan <flaniyanjr@gmail.com>
2023-11-01 18:08:47 -0400

    tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 42, 'M', 'Blue', 'Angry', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 12, 'M', 'Red', 'Sad', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 24, 'F', 'Green', 'Happy', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 88, 'M', 'Red', 'Anxious', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 69, 'F', 'Pink', 'Lazy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 16, 'M', 'Purple', 'Grouchy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'F', 'Yellow', 'Sleepy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 0, 'F', 'Green', 'Unknown', 0);
